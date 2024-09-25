4698efb2708f95b72307fb78e862ccbb94c06ed5
enzzodani <gomes.enzodani@gmail.com>
2024-09-25 06:20:00 -0300

    python day-4 part 1

---
+++ day-4/random_class.py
@@ -0,0 +1,8 @@
+import random
+import my_module
+
+random_int = random.randint(1,10)
+
+print(random_int)
+
+print(my_module.my_fav_number)
---
+++ day-4/random_class2.py
@@ -0,0 +1,9 @@
+import random
+
+random_number_0_to_1 = random.random()
+random_number_0_to_10 = random.random() * 10
+
+random_float = random.uniform(1,10)
+
+print(random_number_0_to_10)
+print(random_float)
