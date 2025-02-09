5196dbb009ffe6f1602a2b1be9d95a9d5cfb75dd
enzzodani <gomes.enzodani@gmail.com>
2025-02-09 03:20:37 -0300

    step3: display almost working

--- day-7/step3.py
+++ day-7/step3.py
@@ -12,26 +12,35 @@ def choice_word():
     for letter in chossen_word:
         temp += " _ "
 
+    return chossen_word, temp
 
-is_complete = 0
 
-while is_complete == 0:
+def win(display_word, chossen_word):
+    if display_word == chossen_word:
+        return 1
+    else:
+        return 0
+
+def game(display_word, chossen_word):
     guess = input("Make a letter guess: ").lower()
 
-    is_complete = 1
     for letter in chossen_word:
         if letter == guess:
+            print(letter)
             display_word += letter
         else:
             display_word += " _ "
-            is_complete = 0
 
-    print(display_word)
 
 def main():
-    display_word = ""
-    choice_word()
+    chossen_word, display_word = choice_word()
+    print(chossen_word)
 
+    while win(display_word, chossen_word) == 0:
+        game(display_word, chossen_word)
+        print(display_word)
+
+    print("You won")
 
 if __name__ == "__main__":
     main()
